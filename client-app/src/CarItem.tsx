896e56bdbad727a71ac481e2b57928aad46c7492
Arnold Mercado <arnold.mercado@outlook.com>
2020-05-22 15:55:26 +0300

    Initial

---
+++ client-app/src/CarItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ICar } from './demo'
+
+
+interface IProps {
+    car: ICar
+}
+export const CarItem : React.FC<IProps> = ({car}) => {
+    return (
+        <div>
+            <h1>{car.model}</h1>
+            <p>{car.color}</p>
+        </div>
+    )
+}

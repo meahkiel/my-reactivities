1d7e8b1edcfa46ae673f3e7c55bcf63ec7db4866
Arnold Mercado <arnold.mercado@outlook.com>
2020-05-22 17:10:00 +0300

    CORS

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { cars } from './demo';
 import { CarItem } from './CarItem';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-		<ul>
-			{cars.map((car) => {
-				<i><CarItem car={car} /></i>
-			})}
-		</ul>
-  </div>
-  )
+class App extends Component {
+	state = {
+		values: []
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:5000/api/values').then((res) => {
+			this.setState({values: res.data});
+			console.log(res);
+		});
+	}
+
+
+	
+	render() {
+
+		const {values} = this.state;
+		
+
+		return (
+			<div className="App">
+				<ul>
+					{values.map((value: any) => <li key={value.id}>{ value.name }</li> )}
+				</ul>
+		  </div>
+		  )
+	}
 }
 
 export default App;
